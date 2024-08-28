592a2c709881c8ccebd10c58122cc146fc64ae47
John Lewis <jslv1995@gmail.com>
2024-08-28 12:02:12 -0400

    Refactor data retrieval functions

--- src/alphavantage_get_data.py
+++ src/alphavantage_get_data.py
@@ -3,52 +3,100 @@ import requests
 from dotenv import load_dotenv
 import json
 
-# Load environment files
-load_dotenv()
+def load_api_key():
+    """Load API key for Alpha Vantage's stock and BTC data retrieval"""
+    # Load environment files
+    load_dotenv()
+    # Access the API key
+    api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
+    # Check if API key is loaded
+    if not api_key:
+        print("API key not found. Please set ALPHA_VANTAGE_API_KEY in your .env file.")
+        return None
+    return api_key
 
-# Access the API key
-api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
-
-# Check if API key is loaded
-if not api_key:
-    print("API key not found. Please set ALPHA_VANTAGE_API_KEY in your .env file.")
-
-# Define function to fetch monthly data
-def fetch_stock_data(symbol):
+def get_stock_data(symbol):
     """Return historical monthly stock data for a publicly traded company."""
+    # Make sure API key is working
+    api_key = load_api_key()
+    if not api_key:
+        return None  # Exit if API key is not loaded
+
+    # Define URL and params
     url = "https://www.alphavantage.co/query"
     params = {
         "function" : "TIME_SERIES_MONTHLY",
         "symbol" : symbol,
         "apikey" : api_key
         }
+    
     # Make the API request
     response = requests.get(url, params=params)
-    # Check status code
-    if response.status_code == 200:
-        # Convert to JSON
-        data = response.json()
-        if "Monthly Time Series" in data:
-            # Store JSON data in a file
-            filename = f"data/stock_data/{symbol}.json"
-            # Check if filename exists and print message
-            if os.path.exists(filename):
-                print(f"File: '{filename}' already exists.. Updating file.")
-            else:
-                print(f"Saving '{filename}' as new file.")
-            # Save the file
-            with open(filename, "w") as json_file:
-                json.dump(data["Monthly Time Series"], json_file, indent=4)
-            # Print success message
-            print(f"Data successfully saved to '{filename}'!")
-            return data["Monthly Time Series"]
+
+    # Check the status code
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return None
+
+    # Convert to JSON
+    data = response.json()
+
+    if "Monthly Time Series" in data:
+        # Store JSON data in a file
+        filename = f"data/stock_data/{symbol}.json"
+        # Check if file exists and print status message
+        if os.path.exists(filename):
+            print(f"File: '{filename}' already exists.. Updating file.")
         else:
-            # Print if error with symbol
-            print(f"No data found for symbol: {symbol}")
-            return None
+            print(f"Saving '{filename}' as new file.")
+        # Save the file
+        with open(filename, "w") as json_file:
+            json.dump(data["Monthly Time Series"], json_file, indent=4)
+        # Print success message
+        print(f"Data successfully saved to '{filename}'!")
+        return data["Monthly Time Series"]
     else:
+        # Print if error with symbol
+        print(f"No data found for symbol: {symbol}")
+        return None
+
+def get_btc_data():
+    """Return historical monthly data for Bitcoin."""
+    # Make sure API key is working
+    api_key = load_api_key()
+    if not api_key:
+        return None  # Exit if API key is not loaded
+
+    # Retrieve BTC data
+    url = "https://www.alphavantage.co/query"
+    params = {
+        "function" : "DIGITAL_CURRENCY_MONTHLY",
+        "symbol" : "BTC",
+        "market" : "USD",
+        "apikey" : api_key
+        }
+    
+    # Make the API request
+    response = requests.get(url, params=params)
+
+    # Check the status code
+    if response.status_code != 200:
         # Print if error with connection to API
         print(f"Error: {response.status_code}")
         return None
 
-monthly = fetch_stock_data("aapl")
+    # Convert to JSON
+    data = response.json()
+    filename = "data/crypto_data/BTC_price_history.json"
+
+    # Check if file exists and print status message
+    if os.path.exists(filename):
+        print(f"File: '{filename}' already exists.. Updating file.")
+    else:
+        print(f"Saving '{filename}' as new file.")
+
+    # Save the file
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+    print(f"Data successfully saved to '{filename}'!")
+    return data  
--- src/coingecko_get_data.py
+++ src/coingecko_get_data.py
@@ -3,31 +3,43 @@ import requests
 from dotenv import load_dotenv
 import json
 
-# Load environment variables from .env file
-load_dotenv()
+def load_api_key():
+    """Load API key for Alpha Vantage's stock and BTC data retrieval"""
+    # Load environment files
+    load_dotenv()
+    # Access the API key
+    api_key = os.getenv("COIN_GECKO_API_KEY")
+    # Check if API key is loaded
+    if not api_key:
+        print("API key not found. Please set COIN_GECKO_API_KEY in your .env file.")
+        return None
+    return api_key
 
-# Access the API key
-api_key = os.getenv("COIN_GECKO_API_KEY")
+def get_companies_holding_btc():
+    """Return data for publicly traded companies holding Bitcoin."""
+    # Make sure API key is working
+    api_key = load_api_key()
+    if not api_key:
+        return None  # Exit if API key is not loaded
+    
+    # Define the endpoint and parameters
+    url = "https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin"
+    params = {
+        "x_cg_demo_api_key": api_key
+        }
 
-# Check if API key is loaded.
-if not api_key:
-    print("API key not found. Please set COIN_GECKO_API_KEY in your .env file.")
+    # Make the API request
+    response = requests.get(url, params=params)
 
-# Define the endpoint and parameters
-url = "https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin"
-params = {
-    "x_cg_demo_api_key": api_key
-    }
+    # Check the status code
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return None
 
-# Make the API request
-response = requests.get(url, params=params)
-
-# Check status code
-if response.status_code == 200:
     # Convert to JSON
     data = response.json()
     # Store JSON data in a file
-    filename = 'data/companies_with_btc.json'
+    filename = 'data/crypto_data/companies_with_btc.json'
     # Check if filename exists and print message
     if os.path.exists(filename):
         print(f"File: '{filename}' already exists.. Updating file.")
@@ -38,6 +50,3 @@ if response.status_code == 200:
         json.dump(data, json_file, indent=4) 
     # Print success message
     print(f"Data successfully saved to '{filename}'!")
-else:
-    # Print if error to connection with API
-    print(f"Error: {response.status_code} - {response.text}")
